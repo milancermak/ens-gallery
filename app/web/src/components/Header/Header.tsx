c5622fb56cdcf2af23cb456a592096bdb06f2073
Milan Cermak <milan.cermak@gmail.com>
2021-10-05 11:15:49 +0200

    style: adding dark header

--- app/web/src/components/Header/Header.tsx
+++ app/web/src/components/Header/Header.tsx
@@ -6,11 +6,13 @@ type HeaderProps = {
 
 const Header = ({ ensInfo }: HeaderProps) => {
   return (
-    <header
-      className="cursor-default m-20 text-3xl font-bold tracking-wide text-center"
-      title={ensInfo.address}
-    >
-      {ensInfo.domain}
+    <header className="flex flex-col w-screen h-20 mb-12 justify-center text-center bg-gray-700">
+      <span
+        className="text-3xl font-bold text-purple-100 tracking-wide cursor-default "
+        title={ensInfo.address}
+      >
+        {ensInfo.domain}
+      </span>
     </header>
   )
 }
