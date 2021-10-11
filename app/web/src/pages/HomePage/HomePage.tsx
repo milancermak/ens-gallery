f95cd750b41307964ea7f0ad55d8b0a87cd7d5b1
Milan Cermak <milan.cermak@gmail.com>
2021-10-11 09:16:05 +0200

    fix: better HTML title

--- app/web/src/pages/HomePage/HomePage.tsx
+++ app/web/src/pages/HomePage/HomePage.tsx
@@ -37,12 +37,7 @@ const HomePage = () => {
 
   return (
     <>
-      <MetaTags
-        title="Home"
-      // description="Home description"
-      /* you should un-comment description and add a unique description, 155 characters or less
-You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
-      />
+      <MetaTags title="NFT Gallery" />
       {ensInfo.address && nfts ? (
         <>
           <Header ensInfo={ensInfo}></Header>
