b3d7e01dc39a86b0a4fc601e1e90aa126e07e478
Milan Cermak <milan.cermak@gmail.com>
2021-10-11 09:57:22 +0200

    feat: lazy loading of NFTAssets

--- app/web/src/components/NftAsset/NftAsset.tsx
+++ app/web/src/components/NftAsset/NftAsset.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 type NftAssetMetadata = {
   collection_name?: string
@@ -15,20 +16,31 @@ export type NftAssetProps = {
 
 const NftAsset = (nft: NftAssetProps) => {
   const [imageFailedToLoad, setImageFailedToLoad] = useState(false)
+  const { ref, inView } = useInView({
+    rootMargin: '200px',
+    threshold: 0,
+  })
 
   if (nft.metadata?.image) {
     return (
-      <div className="relative group w-full rounded z-auto">
+      <div className="relative group w-full rounded z-auto" ref={ref}>
         {imageFailedToLoad ? (
+          // if an image failed to load, we'll try to render an video tag
+
           /* eslint-disable-next-line jsx-a11y/media-has-caption */
           <video src={nft.metadata?.image}></video>
-        ) : (
+        ) : inView ? (
+          // lazy load the images:
+          // when the image is in view, render the img tag,
+          // otherwise show just a simple loading message
           <img
             className="object-scale-down object-center h-full w-full"
             src={nft.metadata.image}
             alt={nft.metadata?.name || 'NFT'}
             onError={() => setImageFailedToLoad(true)}
           />
+        ) : (
+          <p className="h-full w-full">Loading {nft.metadata?.name || 'NFT'}</p>
         )}
         <div className="absolute flex flex-col justify-center select-none w-full bottom-0 h-14 z-10 px-1 opacity-0 transition duration-300 ease-in-out group-hover:bg-gray-100 group-hover:visible group-hover:opacity-90">
           <p className="text-center tracking-wide font-sans font-medium">
